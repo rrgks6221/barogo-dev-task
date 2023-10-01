4c7cb4eccda2d35ec57ce19a91dba07e5723529e
LeeSeokHo <rrgks@naver.com>
2023-10-01 20:40:49 +0900

    [#9]/reafactor: appendCashBodyPipe 수정

--- src/middleware/pipes/cash/append-cash-body.pipe.js
+++ src/middleware/pipes/cash/append-cash-body.pipe.js
@@ -1,5 +1,4 @@
 import { isNil } from '../../../common/functions.js';
-import { AVAILABLE_CASH } from '../../../constants/cash.constant.js';
 import { HTTP_STATUS } from '../../../constants/http-status.constant.js';
 import { CustomException } from '../../../exceptions/custom.exception.js';
 
@@ -18,13 +17,6 @@ export const appendCashBodyPipe = (req, _res, next) => {
     throw new CustomException({
       status: HTTP_STATUS.BAD_REQUEST,
       msg: 'cash 정수형이어야합니다.',
-    });
-  }
-
-  if (!AVAILABLE_CASH[cash]) {
-    throw new CustomException({
-      status: HTTP_STATUS.BAD_REQUEST,
-      msg: '이용가능한 현금이 아닙니다.',
     });
   }
 
--- src/middleware/pipes/cash/append-cash-body.pipe.spec.js
+++ src/middleware/pipes/cash/append-cash-body.pipe.spec.js
@@ -1,5 +1,4 @@
 import { mockNext, mockReq, mockRes } from '../../../../test/mock/http.mock';
-import { AVAILABLE_CASH } from '../../../constants/cash.constant';
 import { CustomException } from '../../../exceptions/custom.exception';
 import { appendCashBodyPipe } from './append-cash-body.pipe';
 
@@ -28,22 +27,6 @@ describe('appendCashBodyPipe', () => {
 
   it('cash 가 정수형이 아닌 경우', () => {
     req.body.cash = 'cash';
-
-    expect(() => appendCashBodyPipe(req, mockRes, mockNext)).toThrowError(
-      CustomException
-    );
-  });
-
-  it('cash 가 이용 불가능한 현금인 경우', () => {
-    req.body.cash = 1;
-
-    expect(() => appendCashBodyPipe(req, mockRes, mockNext)).toThrowError(
-      CustomException
-    );
-  });
-
-  it('cash 가 이용 가능한 현금인 경우', () => {
-    req.body.cash = AVAILABLE_CASH[100];
 
     expect(() => appendCashBodyPipe(req, mockRes, mockNext)).toThrowError(
       CustomException
