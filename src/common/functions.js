29176eaeeb368a15191c5952b56cef0f219ba617
LeeSeokHo <rrgks@naver.com>
2023-10-01 19:22:42 +0900

    [#9]/feat: isNil util function

--- src/common/functions.js
+++ src/common/functions.js
@@ -8,3 +8,11 @@ import { faker } from '@faker-js/faker';
 export const createBeverageStock = (min, max) => {
   return faker.number.int({ min, max });
 };
+
+/**
+ *
+ * @param {*} el
+ */
+export const isNil = (el) => {
+  return el === undefined || el === null;
+};
--- src/common/functions.spec.js
+++ src/common/functions.spec.js
@@ -1,4 +1,4 @@
-import { createBeverageStock } from './functions.js';
+import { createBeverageStock, isNil } from './functions.js';
 
 describe('functions', () => {
   describe('createBeverageStock', () => {
@@ -16,6 +16,24 @@ describe('functions', () => {
       expect(typeof beverage).toBe('number');
       expect(beverage).toBeGreaterThanOrEqual(min);
       expect(beverage).toBeLessThanOrEqual(max);
+    });
+  });
+
+  describe('isNil', () => {
+    it('파라미터가 없는 경우', () => {
+      expect(isNil()).toBe(true);
+    });
+
+    it('undefined 가 들어온 경우', () => {
+      expect(isNil(undefined)).toBe(true);
+    });
+
+    it('null 이 들어온 경우', () => {
+      expect(isNil(null)).toBe(true);
+    });
+
+    it('number type 이 들어온 경우', () => {
+      expect(isNil(100)).toBe(false);
     });
   });
 });
