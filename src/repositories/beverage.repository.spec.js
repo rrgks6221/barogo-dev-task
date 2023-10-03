1b22412efb2f5ab102e8ccdce8a5639de9230a9b
LeeSeokHo <rrgks@naver.com>
2023-10-03 16:29:33 +0900

    [#13]/feat: decreaseStockById

--- src/repositories/beverage.repository.spec.js
+++ src/repositories/beverage.repository.spec.js
@@ -19,4 +19,18 @@ describe('BeverageRepository', () => {
       expect(BeverageRepository.findOneById(-1)).toBeNull();
     });
   });
+
+  describe('decreaseStockById', () => {
+    it('존재하지 않는 음료 업데이트 시', () => {
+      expect(() => BeverageRepository.decreaseStockById(-1)).toThrowError();
+    });
+
+    it('존재하는 음료 업데이트 시', () => {
+      const oldBeverage = BeverageRepository.findAll()[0];
+      const newBeverage = BeverageRepository.decreaseStockById(oldBeverage.id);
+
+      expect(newBeverage).toBeInstanceOf(Beverage);
+      expect(oldBeverage.stock).toBeGreaterThan(newBeverage.stock);
+    });
+  });
 });
